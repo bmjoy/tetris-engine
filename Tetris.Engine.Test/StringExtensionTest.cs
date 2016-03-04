b9026b5cdea876e79c1110085ee8e8897d2522c3
Christoffer Petersen <cb.petersen@gmail.com>
2016-03-04 18:36:39 +0100

    add StringExtensionTest

---
+++ Tetris.Engine.Test/StringExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Tetris.Engine.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        private static readonly bool[][] Expected = { new[] { true, false, true, false }, new [] { true, false, true, false }, new [] { true, false, true, false }, new [] { true, false, true, false } };
+
+        [Test]
+        [TestCase("/da1010101010101010")]
+        [TestCase("1010101010101010")]
+        [TestCase("10asdas10101010101010")]
+        [TestCase(@"1
+0101
+01010101010")]
+        public void StringToBoolMatrixTests(string input)
+        {
+            var result = input.StringToBoolMatrix();
+
+            Assert.AreEqual(Expected.Length, result.Length, "row length");
+            Assert.AreEqual(Expected[0].Length, result[0].Length, "column length");
+
+            for (var rowIndex = 0; rowIndex < input.StringToBoolMatrix().Length; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < Expected[0].Length; columnIndex++)
+                {
+                    Assert.That(result[rowIndex][columnIndex], Is.EqualTo(Expected[rowIndex][columnIndex]));
+                }
+            }
+        }
+    }
+}
--- Tetris.Engine/StringExtension.cs
+++ Tetris.Engine/StringExtension.cs
@@ -7,7 +7,7 @@
     {
         public static bool[][] StringToBoolMatrix(this string str)
         {
-            var bools = new Regex("\\d+").Replace(str, string.Empty).Select(x => x == '1').ToArray();
+            var bools = new Regex("\\D+").Replace(str, string.Empty).Select(x => x == '1').ToArray();
             return new[] {
                              bools.Take(4).ToArray(),
                              bools.Skip(4).Take(4).ToArray(),
